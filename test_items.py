9389064b6875837298ad70c5c9816035f1764ac2
Mendozzzza <110854422+Mendozzzza@users.noreply.github.com>
2022-08-30 22:41:15 +0300

    Update test_items.py
    rename test method

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_items(browser):
+def test_exist_button_buy(browser):
     browser.get(link)
     assert browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
     time.sleep(30)
