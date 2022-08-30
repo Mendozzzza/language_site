01948291c7af2402c3e02120112df314212774e7
Mendozzzza <110854422+Mendozzzza@users.noreply.github.com>
2022-08-30 22:37:54 +0300

    Update test_items.py
    time sleep 30

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_items(browser):
     browser.get(link)
     assert browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
-    time.sleep(10)
+    time.sleep(30)
